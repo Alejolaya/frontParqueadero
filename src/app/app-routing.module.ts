a2b6167034006e620f26bcb40e40cead2770bf2a
Alejolaya <alejolaya@gmail.com>
2019-04-14 10:49:55 -0500

    se agregan botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { RegistrarIngresoComponent } from './registrar-ingreso/registrar-ingreso.component';
 import { AppComponent } from './app.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/AppComponent',pathMatch: 'full'},
-  {path:'AppComponent',component:AppComponent  }
+  {path:'',redirectTo:'/RegistrarIngresoComponent',pathMatch: 'full'},
+  {path:'AppComponent',component:AppComponent  },
+  {path:'RegistrarIngresoComponent',component:RegistrarIngresoComponent  }
 ];
 
 @NgModule({
